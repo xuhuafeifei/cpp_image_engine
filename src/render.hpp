f5f20cecb4dfcca0ab7b448c73d7346b2ccb5713
fgbg <2508020102@qq.com>
2025-02-04 12:40:49 +0800

    支持给不同物体上色

--- src/render.hpp
+++ src/render.hpp
@@ -29,21 +29,31 @@ unsigned char convert(float v)
     return (unsigned char)v;
 }
 
-float sdfSphere(glm::vec3 p, glm::vec3 o, float r)
+glm::vec2 sdfSphere(glm::vec3 p, glm::vec3 o, float r)
 {
-    return glm::length(p - o) - r;
+    // {距离, tag}
+    return {glm::length(p - o) - r, 1};
 }
 
-float sdfGround(glm::vec3 p)
+glm::vec2 sdfGround(glm::vec3 p)
 {
     // 很操蛋, 我的这个y轴是向下为正
     // 所以这里应该是地面坐标 - p.y
-    return - p.y;
+    return {- p.y, 2};
 }
 
-float map(glm::vec3 p)
+glm::vec2 vec2Min(glm::vec2 a, glm::vec2 b) {
+    if (a.x <= b.x) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+glm::vec2 map(glm::vec3 p)
 {
-    return std::min(sdfSphere(p, {circle.x, circle.y, circle.z}, circle.w), sdfGround(p));
+    auto s = sdfSphere(p, {circle.x, circle.y, circle.z}, circle.w);
+    return vec2Min(s, sdfGround(p));
 }
 
 Color fromVec(glm::vec3 v)
@@ -61,7 +71,7 @@ float softshadow( glm::vec3 ro, glm::vec3 rd, float mint, float maxt, float k )
     float t = mint;
     for( int i=0; i<256 && t<maxt; i++ )
     {
-        float h = map(ro + rd*t);
+        float h = map(ro + rd*t).x;
         if( h<0.001 )
             return 0.0;
         res = std::min( res, k*h/t );
@@ -70,20 +80,22 @@ float softshadow( glm::vec3 ro, glm::vec3 rd, float mint, float maxt, float k )
     return res;
 }
 
-float rayMarch(glm::vec3 ro, glm::vec3 rd)
+glm::vec2 rayMarch(glm::vec3 ro, glm::vec3 rd)
 {
-    float t = tMin;
+    float t = 0;
 
     for (int i = 0; i < maxMarchTime && t < tMax; i++)
     {
         glm::vec3 p = ro + rd * t;
-        float d = map(p);
+        auto m = map(p);
+        auto d = m.x;
+        // hit object
         if (d < delta)
-            break;
+            return {t, m.y};
         t += d;
     }
 
-    return t;
+    return {t, -1};
 }
 
 glm::vec3 calcNormal(glm::vec3 p)
@@ -94,15 +106,16 @@ glm::vec3 calcNormal(glm::vec3 p)
     const glm::vec3 v4(1);
     return glm::normalize(
         glm::vec3(
-            map(p + v1 * delta) * v1 +
-            map(p + v2 * delta) * v2 +
-            map(p + v3 * delta) * v3 +
-            map(p + v4 * delta) * v4));
+            map(p + v1 * delta).x * v1 +
+            map(p + v2 * delta).x * v2 +
+            map(p + v3 * delta).x * v3 +
+            map(p + v4 * delta).x * v4));
 }
 
 float normalShadow(glm::vec3 p, glm::vec3 light)
 {
-    float t = rayMarch(p, glm::normalize(light - p));
+    auto rm = rayMarch(p, glm::normalize(light - p));
+    auto t = rm.x;
     if (t < tMax)
     {
         return 0.1;
@@ -119,10 +132,12 @@ Color render(int x, int y)
     auto ray = cam.ray(uv);
     glm::vec3 light = glm::vec3(10, -15, 7);
 
-    float t = rayMarch(ray.ro, ray.rd);
+    auto rm = rayMarch(ray.ro, ray.rd);
 
-    if (t < tMax)
+    // rm.y = -1时, 表示没有hit object
+    if (rm.y > 0)
     {
+        auto t = rm.x;
         glm::vec3 p = ray.at(t);
         glm::vec3 n = calcNormal(p);
         float diff = glm::dot(
@@ -136,6 +151,11 @@ Color render(int x, int y)
         // color = glm::vec3(1) * diff + amb * glm::vec3(0.5);
         float amb = 0.1;
         color = glm::vec3(1) * diff + amb;
+        if (rm.y == 1) {
+            color += glm::vec3(1, 0, 0);
+        } else if (rm.y == 2) {
+            color += glm::vec3(0, 0, 1);
+        }
     }
     return fromVec(color);
 }
